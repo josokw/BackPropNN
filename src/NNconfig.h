825c2487a44f707fdeae609c24c00ae9bdebcb1b
josWSL2 <jos.onokiewicz@han.nl>
2020-06-01 12:23:35 +0200

    Remove white space

--- src/NNconfig.h
+++ src/NNconfig.h
@@ -4,7 +4,7 @@
 ///< Momentum, multiplier of last deltaWeight, [0.0..1.0]
 const double ALFA = 0.5;
 ///< Overall net learning rate, [0.0..1.0]
-const double ETA = 0.15; 
+const double ETA = 0.15;
 const int MAX_ITERATIONS = 100'000;
 const double MIN_RECENT_AVERAGE_ERROR = 0.01;
 
